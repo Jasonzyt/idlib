bc2963e756f9ada7d9a86552e3e02bc005bc9738
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2023-09-27 04:21:53 +0800

    feat: add do_mod11_2_int

--- src/mod11-2.h
+++ src/mod11-2.h
@@ -5,10 +5,9 @@ namespace idlib {
 
 namespace mod11_2 {
 
-constexpr char kFactors[] = {7, 9, 10, 5,  8, 4, 2, 1, 6,
-                             3, 7, 9,  10, 5, 8, 4, 2};
-constexpr char kCheckDigits[] = {'1', '0', 'X', '9', '8', '7',
-                                 '6', '5', '4', '3', '2'};
+constexpr char kFactors[] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+constexpr char kCheckDigits[] = {'1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'};
+constexpr int kCheckInts[] = {1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2};
 
 constexpr char do_mod11_2(char id[18]) {
     for (int i = 0; i < 16; ++i) {
@@ -38,6 +37,22 @@ constexpr char do_mod11_2(std::string_view id) {
         sum += (id[i] - '0') * kFactors[i];
     }
     return kCheckDigits[sum % 11];
+}
+
+constexpr int do_mod11_2_int(std::string_view id) {
+    if (id.size() != 17) {
+        return -1;
+    }
+    for (int i = 0; i < 16; ++i) {
+        if (id[i] < '0' || id[i] > '9') {
+            return -1;
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < 17; ++i) {
+        sum += (id[i] - '0') * kFactors[i];
+    }
+    return kCheckInts[sum % 11];
 }
 
 } // namespace mod11_2
