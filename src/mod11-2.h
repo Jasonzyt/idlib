23334ac1a112cb7298dcc27de3f33a474bbce68e
Jasonzyt <66063199+Jasonzyt@users.noreply.github.com>
2023-09-27 01:36:07 +0800

    fix: fix mod11-2

--- src/mod11-2.h
+++ src/mod11-2.h
@@ -10,7 +10,7 @@ constexpr char kFactors[] = {7, 9, 10, 5,  8, 4, 2, 1, 6,
 constexpr char kCheckDigits[] = {'1', '0', 'X', '9', '8', '7',
                                  '6', '5', '4', '3', '2'};
 
-constexpr char do_mod11_2(char id[17]) {
+constexpr char do_mod11_2(char id[18]) {
     for (int i = 0; i < 16; ++i) {
         if (id[i] < '0' || id[i] > '9') {
             return -1;
@@ -28,11 +28,16 @@ constexpr char do_mod11_2(std::string_view id) {
     if (id.size() != 17) {
         return -1;
     }
-    char id_array[17];
-    for (int i = 0; i < 17; ++i) {
-        id_array[i] = id[i];
+    for (int i = 0; i < 16; ++i) {
+        if (id[i] < '0' || id[i] > '9') {
+            return -1;
+        }
     }
-    return do_mod11_2(id_array);
+    int sum = 0;
+    for (int i = 0; i < 17; ++i) {
+        sum += (id[i] - '0') * kFactors[i];
+    }
+    return kCheckDigits[sum % 11];
 }
 
 } // namespace mod11_2
